b44901d911c5ae4156dec8b861621d0efaf03398
Jose Antonio Fernández Salado <josanfersal@gmail.com>
2021-05-10 17:29:20 +0200

    1.0

---
+++ GastosEmpresa/GastosEmpresaHome.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GastosEmpresa
+{
+    /// <summary>
+    /// Lógica de interacción para GastosEmpresaHome.xaml
+    /// </summary>
+    public partial class GastosEmpresaHome : Page
+    {
+        public GastosEmpresaHome()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            // Ver Informe de Gastos
+            InformeGastos informeGastos = new InformeGastos(this.ListaEmpleados.SelectedItem);
+            this.NavigationService.Navigate(informeGastos);
+
+        }
+    }
+}
---
+++ GastosEmpresa/InformeGastos.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace GastosEmpresa
+{
+    /// <summary>
+    /// Lógica de interacción para InformeGastos.xaml
+    /// </summary>
+    public partial class InformeGastos : Page
+    {
+        public InformeGastos()
+        {
+            InitializeComponent();
+        }
+
+
+        // Constructor para pasar los datos de gastos
+        public InformeGastos(object data) : this()
+        {
+            // Enlaza con el informe de datos
+            this.DataContext = data;
+        }
+
+
+    }
+}
